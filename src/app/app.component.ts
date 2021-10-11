08cfac34fbe537eb535d207773ae65e6b3e1230a
maau <Marc.Aumueller@EXXETA.com>
2021-10-11 07:46:06 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hochzeit';
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
+  }
 }
